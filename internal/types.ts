d6e2a2b55661c4615da61ce83bbb3cff809ff9c5
Gabriel Harel <gabrielj.harel@gmail.com>
2021-06-13 11:51:31 -0500

    add more docs to types

--- internal/types.ts
+++ internal/types.ts
@@ -1,17 +1,28 @@
+// Keycap set with matching theme.
 export interface Keyset {
+    name: string; // TODO vendor/product id.
+
+    // Group of keycaps available as a unit.
     kits: KeysetKit[];
 }
 
+// Keyboard layout.
 export interface Layout {
+    // Keys that do not overlap and not part of an option.
     fixedKeys: LayoutKey[];
+
+    // Layout options for this layout.
     options: LayoutOption[];
 }
 
 //
 
+// Key placement options for an overlapping section of the layout.
 export interface LayoutOption {
     name: string;
-    options: LayoutKey[][]; // All options should overlap exactly.
+
+    // All options should overlap exactly.
+    options: LayoutKey[][];
 }
 
 export interface LayoutKey {
