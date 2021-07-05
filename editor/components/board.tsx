66237c916eb1701eff2678d8dcb4b322ad817e39
g-harel <gabrielj.harel@gmail.com>
2021-07-05 11:15:08 -0500

    start converting editor to svg

--- editor/components/board.tsx
+++ editor/components/board.tsx
@@ -24,11 +24,55 @@ const KeyTransform = styled.div`
     height: 0;
 `;
 
+const Group = styled.g`
+    :hover {
+        rect {
+            fill: blue;
+        }
+    }
+`;
+
 export const Board: React.FunctionComponent<IProps> = ({layout, width}) => {
     const [min, max] = minmax(layout);
     const unitWidth = max.x - min.x;
     const unitHeight = max.y - min.y;
     const unit = width / unitWidth;
+
+    const svg = (
+        <svg
+            xmlnls="http://www.w3.org/2000/svg"
+            viewBox={`0 0 ${unitWidth} ${unitHeight}`}
+            width={width}
+        >
+            {layout.fixedKeys.map((fixedKey, i) => (
+                <Group
+                    key={i}
+                    style={{
+                        transform: `rotate(${fixedKey.angle}deg) translate(${
+                            fixedKey.position.x - min.x
+                        }px,${fixedKey.position.y - min.y}px)`,
+                        transformOrigin: `${-min.x}px ${-min.y}px`,
+                    }}
+                >
+                    {fixedKey.key.shape.map((shape, j) => (
+                        <rect
+                            key={j}
+                            fill="red"
+                            stroke="black"
+                            x={shape.offset.x}
+                            y={shape.offset.y}
+                            strokeWidth="0.01"
+                            rx="0.02"
+                            width={shape.width}
+                            height={shape.height}
+                        />
+                    ))}
+                </Group>
+            ))}
+        </svg>
+    );
+
+    if (Math.random() < 1.0) return svg;
 
     const [selected, setSelected] = useState<Record<string, boolean>>({});
     const toggleSelected = (id: number) => () => {
