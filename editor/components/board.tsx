f85bd500982c6f7e45370399e6dc365960ab54e2
g-harel <gabrielj.harel@gmail.com>
2021-07-07 11:47:57 -0500

    add multi shape border logic to svg blanks

--- editor/components/board.tsx
+++ editor/components/board.tsx
@@ -2,27 +2,13 @@ import React, {useState} from "react";
 import styled from "styled-components";
 
 import {minmax} from "../../internal/layout";
-import {Coord} from "../../internal/types/base";
+import {Blank} from "../../internal/types/base";
 import {Layout} from "../../internal/types/layout";
-import {Key} from "./key";
 
 export interface IProps {
     width: number;
     layout: Layout;
 }
-
-const Wrapper = styled.div<IProps>`
-    padding: 10px;
-
-    /* 1u */
-    font-size: 2rem;
-`;
-
-const KeyTransform = styled.div`
-    position: relative;
-    width: 0;
-    height: 0;
-`;
 
 const Group = styled.g`
     :hover {
@@ -32,13 +18,60 @@ const Group = styled.g`
     }
 `;
 
+const FillRect = styled.rect``;
+
+const StrokeRect = styled.rect``;
+
+const ShapeWrapper = styled.g`
+    ${FillRect} {
+        fill: red;
+    }
+
+    ${StrokeRect} {
+        fill: black;
+    }
+`;
+
+const PAD = 0.03;
+const BORDER = 0.02;
+
+export const Shape: React.FunctionComponent<Blank> = (props) => (
+    <ShapeWrapper>
+        {props.shape.map((shape, j) => (
+            <StrokeRect
+                key={j}
+                x={shape.offset.x + PAD}
+                y={shape.offset.y + PAD}
+                rx="0.02"
+                width={shape.width - 2 * PAD}
+                height={shape.height - 2 * PAD}
+            />
+        ))}
+        {props.shape.map((shape, j) => (
+            <FillRect
+                key={j}
+                x={shape.offset.x + PAD + BORDER}
+                y={shape.offset.y + PAD + BORDER}
+                rx="0.02"
+                width={shape.width - 2 * (PAD + BORDER)}
+                height={shape.height - 2 * (PAD + BORDER)}
+            />
+        ))}
+    </ShapeWrapper>
+);
+
 export const Board: React.FunctionComponent<IProps> = ({layout, width}) => {
     const [min, max] = minmax(layout);
     const unitWidth = max.x - min.x;
     const unitHeight = max.y - min.y;
     const unit = width / unitWidth;
 
-    const svg = (
+    const [selected, setSelected] = useState<Record<string, boolean>>({});
+    const toggleSelected = (id: number) => () => {
+        setSelected(Object.assign({}, selected, {[id]: !selected[id]}));
+    };
+
+    return (
         <svg
             xmlnls="http://www.w3.org/2000/svg"
             viewBox={`0 0 ${unitWidth} ${unitHeight}`}
@@ -54,62 +87,9 @@ export const Board: React.FunctionComponent<IProps> = ({layout, width}) => {
                         transformOrigin: `${-min.x}px ${-min.y}px`,
                     }}
                 >
-                    {fixedKey.key.shape.map((shape, j) => (
-                        <rect
-                            key={j}
-                            fill="red"
-                            stroke="black"
-                            x={shape.offset.x}
-                            y={shape.offset.y}
-                            strokeWidth="0.01"
-                            rx="0.02"
-                            width={shape.width}
-                            height={shape.height}
-                        />
-                    ))}
+                    <Shape {...fixedKey.key} key={i} />
                 </Group>
             ))}
         </svg>
-    );
-
-    if (Math.random() < 1.0) return svg;
-
-    const [selected, setSelected] = useState<Record<string, boolean>>({});
-    const toggleSelected = (id: number) => () => {
-        setSelected(Object.assign({}, selected, {[id]: !selected[id]}));
-    };
-
-    return (
-        <Wrapper
-            style={{
-                width: unitWidth + "em",
-                height: unitHeight + "em",
-                fontSize: unit + "px",
-            }}
-        >
-            {layout.fixedKeys.map((fixedKey, i) => {
-                const origin: Coord = {
-                    x: -fixedKey.position.x + min.x,
-                    y: -fixedKey.position.y + min.y,
-                };
-                return (
-                    <KeyTransform
-                        key={i}
-                        style={{
-                            left: fixedKey.position.x - min.x + "em",
-                            top: fixedKey.position.y - min.y + "em",
-                            transform: `rotate(${fixedKey.angle}deg)`,
-                            transformOrigin: `${origin.x}em ${origin.y}em`,
-                        }}
-                    >
-                        <Key
-                            blank={fixedKey.key}
-                            onClick={toggleSelected(i)}
-                            selected={selected[i]}
-                        />
-                    </KeyTransform>
-                );
-            })}
-        </Wrapper>
     );
 };
