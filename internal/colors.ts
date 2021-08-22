75d730aec33451cd92108528a2384bc06c16ccac
g-harel <gabrielj.harel@gmail.com>
2021-08-22 14:24:42 -0500

    use best guess color codes for keyset

--- internal/colors.ts
+++ internal/colors.ts
@@ -1,10 +1,10 @@
 export const resolveColor = (hexOrCode: string): string => {
     switch (hexOrCode) {
-        case "WS1":
+        case "GR21": // Botanical white.
             return "#d3d7da";
-        case "botanical-light-green":
+        case "GN21": // Botanical light green.
             return "#9fb5ab";
-        case "botanical-dark-green":
+        case "GN20": // Botanical dark green.
             return "#4a6056";
         case "botanical-beige":
             return "#c9c6bd";
