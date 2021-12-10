000012e4e92e476bb5fb67e755c869eda039cf66
Gabriel Harel <gabrielj.harel@gmail.com>
2021-12-09 19:08:46 -0600

    move header to components

---
+++ editor/components/header.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+
+import {Logo} from "./components/logo";
+
+const StyledHeader = styled.header`
+    align-items: center;
+    border-bottom: 2px solid lightpink;
+    display: flex;
+    flex-direction: row;
+    flex-shrink: 0;
+    height: 3rem;
+    justify-content: space-between;
+    background-color: pink;
+`;
+
+const StyledLogoLink = styled(Link)`
+    padding: 0.2rem;
+    border-radius: 0.2rem;
+    margin-left: 0.5rem;
+
+    :hover {
+        background-color: lightpink;
+    }
+
+    :active {
+        opacity: 0.8;
+    }
+`;
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <StyledLogoLink to="/">
+                <Logo color="lightcoral" size="2rem" />
+            </StyledLogoLink>
+            <Link to="/account">Account</Link>
+        </StyledHeader>
+    );
+};
