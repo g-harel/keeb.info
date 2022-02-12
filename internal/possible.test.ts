0c74e5cc29a80415405810de8b070c2a314fa3f9
Gabriel Harel <gabrielj.harel@gmail.com>
2022-02-12 07:53:59 -0600

    add more tests to possible lib

--- internal/possible.test.ts
+++ internal/possible.test.ts
@@ -1,7 +1,16 @@
-import {Err} from "./possible";
+import {Err, Possible} from "./possible";
 
 describe("Err", () => {
     // TODO make table of values.
+    it("Should assert the type.", () => {
+        const testValue = {test: true};
+        const possibleErr: Possible<typeof testValue> = testValue;
+
+        if (Err.isErr(possibleErr)) {
+            expect(Err.isErr(possibleErr)).toBeFalsy();
+        }
+        expect(possibleErr.test).toBeTruthy(); // Type check.
+    });
 
     it("Should detect errors.", () => {
         const err = Err.err("test");
@@ -16,5 +25,24 @@ describe("Err", () => {
     it("Should detect elaborated errors.", () => {
         const err = Err.err("test").with("test");
         expect(Err.isErr(err)).toBeTruthy();
+    });
+
+    it("Should inlcude message in printed errors.", () => {
+        const testString = "abc";
+        const err = Err.err(testString);
+
+        expect(err.print()).toContain(testString);
+    });
+
+    it("Should include extended messages in printed errors.", () => {
+        const testStrings = ["foo", "bar", "xyz"];
+        let err = Err.err(testStrings[0]);
+        for (let i = 1; i < testStrings.length; i++) {
+            err = err.with(testStrings[i]);
+        }
+
+        for (const message of testStrings) {
+            expect(err.print()).toContain(message);
+        }
     });
 });
