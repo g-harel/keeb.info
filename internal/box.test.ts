ee26fbad5861137e4e9cedeac93da930ebfa56a7
g-harel <gabrielj.harel@gmail.com>
2022-03-10 00:23:01 -0600

    add test for toShape helper

--- internal/box.test.ts
+++ internal/box.test.ts
@@ -1,4 +1,5 @@
-import {Box, toComposite} from "./box";
+import {Box, toComposite, toShape} from "./box";
+import {Err} from "./possible";
 
 describe("toComposite", () => {
     it("should combine touching boxes", () => {
@@ -26,5 +27,34 @@ describe("toComposite", () => {
         ];
         const composite = toComposite(boxes);
         expect(composite[0]).toHaveLength(8);
+    });
+});
+
+describe("toShape", () => {
+    it("should combine touching boxes", () => {
+        const boxes: Box[] = [
+            {height: 1, width: 1, offset: [0, 0]},
+            {height: 1, width: 1, offset: [1, 0]},
+        ];
+        const composite = toShape(boxes);
+        expect(composite).toHaveLength(4);
+    });
+
+    it("should not support split boxes", () => {
+        const boxes: Box[] = [
+            {height: 1, width: 1, offset: [0, 0]},
+            {height: 1, width: 1, offset: [0, 2]},
+        ];
+        const composite = toShape(boxes);
+        expect(Err.isErr(composite)).toBeTruthy();
+    });
+
+    it("should combine complex touching boxes", () => {
+        const boxes: Box[] = [
+            {height: 1, width: 1, offset: [0, 0]},
+            {height: 1, width: 1, offset: [0.5, 0.5]},
+        ];
+        const composite = toShape(boxes);
+        expect(composite).toHaveLength(8);
     });
 });
--- internal/box.ts
+++ internal/box.ts
@@ -23,7 +23,6 @@ export const toComposite = (boxes: Box[]): Composite => {
     return multiUnion(...shapes);
 };
 
-// TODO error reporting with possible
 export const toShape = (boxes: Box[]): Possible<Shape> => {
     const m = toComposite(boxes);
     if (m.length === 0) return [];
