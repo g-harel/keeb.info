152b1b8432ca09b000f013b6b5fc3897fc702229
Gabriel Harel <gabrielj.harel@gmail.com>
2022-03-14 22:48:35 -0500

    attempt to force use of .err

--- internal/possible.ts
+++ internal/possible.ts
@@ -9,7 +9,7 @@ export const newErr = (message: string): ErrContainer => {
     return container;
 };
 
-export const isErr = <T>(value: Possible<T>): value is ErrContainer => {
+export const isErr = <T>(value: T | UnresolvedErrContainer): value is UnresolvedErrContainer => {
     return (
         value &&
         (value as any).err &&
@@ -17,15 +17,42 @@ export const isErr = <T>(value: Possible<T>): value is ErrContainer => {
     );
 };
 
-export class ErrContainer {
+export interface UnresolvedErrContainer {
+    err: Err;
+}
+
+export class ErrContainer  {
     private $__internal__identity__ = internalIdentity;
     public err!: Err;
 }
+
+// TODO TESTING START
+const a = (): Possible<string> => {
+    return "";
+}
+const b = (): Possible<string> => {
+    const aa = a();
+    if (isErr(aa)) {
+        if (Math.random() > 0.5) {
+            return aa.err.with("test");
+        }
+        const aaa = aa.err.forward();
+        return aa;
+    }
+    return "";
+};
+// TODO TESTING END
 
 // TODO add error type and type checking (ex. NotFoundErr)
 // TODO force "value.err" to be used even when returning plain err.
 class Err {
     private messages: string[] = [];
+
+    public forward(): ErrContainer {
+        const container = new ErrContainer();
+        container.err = this;
+        return container;
+    }
 
     public with(message: string): ErrContainer {
         const container = new ErrContainer();
