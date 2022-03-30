7c8c6c5be79508878b1b3e19619671bb99c4dbb9
Gabriel Harel <gabrielj.harel@gmail.com>
2022-03-30 13:22:53 -0500

    actual index wrapper

--- internal/search.ts
+++ internal/search.ts
@@ -1,16 +1,48 @@
 import lunr from "lunr";
 
-// TODO wrap index type.
+// TODO use this.
+// TODO add query methods.
+export class Search<T> {
+    private index: lunr.Index;
 
-export const serializedIndex = <T extends object>(
+    private constructor(index: lunr.Index) {
+        this.index = index;
+    }
+
+    public static fromDocuments<T>(
+        documents: T[],
+        refField: string,
+        fields: string[],
+    ): Search<T> {
+        const idx = lunr(function () {
+            const $idx = this;
+
+            $idx.ref(refField);
+            fields.forEach((field) => $idx.field(field));
+            documents.forEach((doc) => $idx.add(doc as any));
+        });
+
+        return new Search(idx);
+    }
+
+    public static fromSerialized<T>(idx: string): Search<T> {
+        return new Search(lunr.Index.load(JSON.parse(idx)));
+    }
+
+    public serialize(): string {
+        return JSON.stringify(this.index);
+    }
+}
+
+export const createSerializedIndex = <T extends object>(
     documents: T[],
-    ref: string,
+    refField: string,
     fields: string[],
 ): string => {
     const idx = lunr(function () {
         const $idx = this;
 
-        $idx.ref(ref);
+        $idx.ref(refField);
         fields.forEach((field) => $idx.field(field));
         documents.forEach((doc) => $idx.add(doc));
     });
