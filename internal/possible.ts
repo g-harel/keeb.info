88436f3ab3408c93ca40e01e36d476ffd52a1fec
Gabriel Harel <gabrielj.harel@gmail.com>
2022-05-24 20:30:18 -0500

    start adding docs to public possible api

--- internal/possible.ts
+++ internal/possible.ts
@@ -3,7 +3,7 @@ export type AsyncPossible<T> = Promise<Possible<T>>;
 
 const GLOBAL_ERR_IDENTITY = {};
 
-// TODO 2022-05-20 internal Err wrapper for typesafe access (remove all "as any")
+// TODO 2022-05-20 internal Err wrapper for type-safe access (remove all "as any")
 
 // TODO 2022-05-20 builder that works better with try/catch
 export const newErr = (message: string): Err => {
@@ -16,24 +16,27 @@ export const isErr = (value: any): value is Err | UnresolvedErr => {
     );
 };
 
-// TODO 2022-05-20 should this also traverse err ancestry?
-// TODO 2022-05-20 should this restrict err ancestry depth to 0?
+// Type guard that asserts `value` is an `Er`r that matches the matcher type.
+// All the ancestor types of the `value` will be compared to only the most
+// recent identiy of the `matcher`.
 export const isErrOfType = (
     value: any,
-    err: Err,
+    matcher: Err,
 ): value is Err | UnresolvedErr => {
     if (!isErr(value)) return false;
     return (value as any)
         .nextErrs()
-        .find((e: Err) => (e as any).$identity === (err as any).$identity);
+        .find((e: Err) => (e as any).$identity === (matcher as any).$identity);
 };
 
+// Internal helper that captures the public `Err` API without the .err member.
+// It is used to restrict repeated chains of .err.err...
 interface IErr {
     decorate: (messageOrErr: string | Err) => Err;
     print: () => string;
 }
 
-// To dissalow confusing calls to "isErr" guarded Possible values.
+// To disallow confusing calls to `isErr` guarded Possible values.
 //     <allowed> possibleValue.err.print();
 // <not allowed> possibleValue.print();
 // <not allowed> possibleValue.err.err;
